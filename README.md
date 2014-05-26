a1ff7de5bc83055f80073b537664360c970ac551
David Moreau <david@prowebserver.fr>
2014-05-26 16:49:21 +0200

    Initial code

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-formidation
+Formidation
 ===========
 
-Standalone validation form library, inspired by CodeIgniter's one
+Standalone validation form library, inspired by CodeIgniter's one.
+
+Usage
+-----
+
+Just include the file /Formidation.php/ and use it like in /test/ directory. You *must* have /inc/, /lang/, /filters/ and /rules/ in the same directory.
+Comments and documentation are in french. You can contact me if you want that in english.
---
+++ test/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Formulaire de test</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form action="action.php" method="post">
+           <label>Nom : </label> <input type="text" name="nom" /><br />
+           <label>Prénom : </label> <input type="text" name="prenom" /><br />
+           <label>Âge : </label><input type="number" name="age" /><br />
+           <input type="submit" />
+        </form>
+    </body>
+</html>
